1efcd128aecb9d886d4a62071f8cee2fc1bfac82
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-04-20 00:38:41 -0300

    Initial commit

---
+++ src/main/java/com/desafio/sportTechdev2024/SportTechDev2024Application.java
@@ -0,0 +1,13 @@
+package com.desafio.sportTechdev2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTechDev2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTechDev2024Application.class, args);
+	}
+
+}
